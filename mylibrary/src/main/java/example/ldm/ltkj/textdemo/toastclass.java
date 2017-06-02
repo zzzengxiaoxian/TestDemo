e97781d50325834e449b7801cee2852a928d4023
liudongming <928012536@qq.com>
2017-06-02 16:15:27 +0800

    1

---
+++ mylibrary/src/main/java/example/ldm/ltkj/textdemo/toastclass.java
@@ -0,0 +1,24 @@
+package example.ldm.ltkj.textdemo;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 吐司工具类
+ *
+ * @author chenjunxu
+ * @date 16/8/30
+ */
+public class toastclass {
+
+    /**
+     * 显示吐司
+     *
+     * @param context
+     * @param str
+     */
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+}
